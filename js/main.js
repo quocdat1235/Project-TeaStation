528380cdcebd5deaf14c665ce882e00e544b0353
Nguyen Van Quoc Dat <vandat13062005@gmail.com>
2026-05-27 23:13:44 +0700

    hoàn thành nửa dự án

--- js/main.js
+++ js/main.js
@@ -1,13 +1,47 @@
-import {partnerLogos, productList, partnerLogoBasePath} from "./data.js";
+import { partnerLogos, productList, partnerLogoBasePath } from "./data.js";
+
+/*========================== Navigation ==========================*/
+$(function () {
+  // hide show nav
+  $(".navbar").hidescroll();
+
+
+  // mobile dropdown menu
+  const toggleBtn = $("#toggle_btn");
+  const drodownMenu = $(".dropdown-menu");
+
+  toggleBtn.click(() => {
+    drodownMenu.toggleClass("open");
+  })
+});
+
+
+
 
 /*========================== Partner Logos ==========================*/
-$(function() {
-    const container = document.getElementById("partner-logo-list");
-    partnerLogos.forEach((logo) => {
-        const img = document.createElement("img");
-        img.src = partnerLogoBasePath + logo.fileName;
-        img.alt = logo.alt;
-        img.classList.add("logo-ticker-image");
-        container.appendChild(img);
-    });
+$(function () {
+  const container = document.getElementById("partner-logo-list");
+  partnerLogos.forEach((logo) => {
+    const img = document.createElement("img");
+    img.src = partnerLogoBasePath + logo.fileName;
+    img.alt = logo.alt;
+    img.classList.add("logo-ticker-image");
+    container.appendChild(img);
+  });
+});
+
+/*========================== Products ==========================*/
+$(function () {
+  // thêm activeTab vào li đầu
+  $("li:first").addClass("activeTab");
+
+  // đổi màu activeTab
+  $("li").on("click", function () {
+    $("li").removeClass("activeTab");
+    $('div[id="products-tabs"] ul .r-tabs-state-active').addClass("activeTab");
+  });
+
+  $("#products-tabs").responsiveTabs({
+    animation: "slide",
+  });
 });
